2e14be2aa1f80e6677420ade1915bfb1ca9f3905
Jose Cordoba <jmcordoba@universidadean.edu.co>
2019-05-25 10:42:42 -0500

    Creado el archivo de ejemplo de php

---
+++ tutoria_3/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hola php</title>
+</head>
+<body>
+    
+    <h2>Hola desde html</h2>
+    <h2><?php 
+        
+        $entero = 10;
+        $flotantes = 12.3;
+        $cadena = 'Hola mundo';
+        $booleano = true;
+        
+        echo 'la suma entre '.$entero.' y '. $flotantes. ' es '. ($entero + $flotantes);
+        
+        
+        ?></h2>
+    
+</body>
+</html>
