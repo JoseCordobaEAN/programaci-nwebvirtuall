52dab1580ec53463e77c11992973e496c9532283
Jose Cordoba <jmcordoba@universidadean.edu.co>
2019-06-09 13:21:09 -0500

    Se agrega la funcionalidad de listar usuarios

--- encuentro/respuesta.php
+++ encuentro/respuesta.php
@@ -4,8 +4,8 @@ require('bd.php');
 
 
 ?>
-    <p>
-       <?php
+<p>
+    <?php
         if (isset($_POST['usuario']) && isset($_POST['pass'])){
 
             $query = "INSERT INTO usuario ( nombre, contrasena) VALUES ('".$_POST['usuario']."', '".$_POST['pass']."')";
@@ -15,7 +15,7 @@ require('bd.php');
             if ($conexion -> query($query)){
                 echo '<br>Se Insertó el usuario';
             }else{
-                echo '<br>paila';
+                echo '<br>paila ese man no se deja';
             }
 
 
@@ -23,7 +23,46 @@ require('bd.php');
             echo 'Nada';
         }
         ?>
-    </p>
+</p>
+
+
+<div class="container">
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">ID</th>
+            <th scope="col">NOMBRE</th>
+            <th scope="col">CONTRASEÑA</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+
+        $query = 'SELECT * FROM usuario';
+
+        $resultado = $conexion -> query($query);
+
+         if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo '<tr>
+                        <th scope="row">'.$row['id'].'</th>
+                        <td>'.$row['id'].'</td>
+                        <td>'.$row['nombre'].'</td>
+                        <td>'.$row['contrasena'].'</td>
+                    </tr>';
+            }
+        } else {
+            echo "0 results";
+        }
+
+        ?>
+
+    </tbody>
+</table>
+</div>
+
+
 <?php
 require('footer.php');
 ?>
